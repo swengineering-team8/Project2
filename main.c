163f83278ce36b4876cd6315b526a66fe23fce8a
KYH <yeong3281@gmail.com>
2022-05-10 15:28:45 +0900

    Write filetime1 and filetime2

--- main.c
+++ main.c
@@ -38,19 +38,27 @@ void filestat1(){
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
     	char path[] = "text2";
-	int res = stat(path,&stat1);
+	int res = stat(path,&stat2);
 	if(res == -1)
 		printf("Erorr: %s exists.\n", path);
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    
+     	time1=localtime(&stat1.st_mtime); 
+        printf("%04d-%02d-%02d %02d:%02d\n", 
+                time1->tm_year+1900, time1->tm_mon+1, 
+                time1->tm_mday, time1->tm_hour, 
+                time1->tm_min);   
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    
+            time2=localtime(&stat2.st_mtime); 
+        printf("%04d-%02d-%02d %02d:%02d\n", 
+                time2->tm_year+1900, time2->tm_mon+1, 
+                time2->tm_mday, time2->tm_hour, 
+                time2->tm_min);
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
