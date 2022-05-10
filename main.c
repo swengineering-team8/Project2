5e2fca88256e40041aa19afdac0201a353b5384a
can13any <mnvyiions@gmail.com>
2022-05-10 19:00:58 +0900

    write sizecmp()

--- main.c
+++ main.c
@@ -63,7 +63,18 @@ void filetime2(){
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    
+    printf("\n");
+    printf("size compare\n");
+    if((int)stat1.st_size > (int)stat2.st_size){
+        printf("text1 is bigger\n"); 
+    }
+    else if((int)stat1.st_size < (int)stat2.st_size){
+        printf("text2 is bigger\n"); 
+    }
+    else{
+        printf("sizes are equal\n");         
+    }
+    printf("\n");
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
